e55d57c72685f04d608edf007810ee4f27c58d7c
yasminegharbii <jasminenotdead@gmail.com>
2026-03-03 20:29:14 +0100

    js

---
+++ s.js
@@ -0,0 +1,18 @@
+
+document.getElementsByTagName('div');
+
+document.getElementById('list');
+
+
+
+
+
+
+var items = document.querySelectorAll('li');
+
+items.forEach(function(item) {
+  item.textContent += " Hello World";
+});
+
+
+
